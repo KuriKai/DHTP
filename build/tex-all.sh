c23324c6b282e1534f4d9c94e5ebc50f067474f2
dave <eunbolt@gmail.com>
2016-10-17 18:45:17 +1300

    updated zdoom build script and changed order of tex-all so it doens't fail on zdoom building

--- build/tex-all.sh
+++ build/tex-all.sh
@@ -6,8 +6,8 @@ base_path=$(dirname "$script_path")
 echo ----Starting Doomsday pack----
 source $script_path/tex-deng.sh
 
-echo ----starting Risen3d pack----
-source $script_path/tex-r3d.sh
-
 echo ----starting zdoom pack----
 source $script_path/tex-zd.sh
+
+echo ----starting Risen3d pack----
+source $script_path/tex-r3d.sh
--- build/tex-zd.sh
+++ build/tex-zd.sh
@@ -9,35 +9,21 @@ rm -r $base_path/zdoom
 mkdir $base_path/built
 
 echo ---------starting to create the dhtp for the zdoom engine and compatible engines---------
-mkdir -p $base_path/zdoom/doom/hires
-mkdir -p $base_path/zdoom/doom2/hires
-mkdir -p $base_path/zdoom/doom2.plutonia/hires
-mkdir -p $base_path/zdoom/doom2.tnt/hires
+mkdir -p $base_path/zdoom/filter/doom/hires
+mkdir -p $base_path/zdoom/filter/doom.doom2/hires
+mkdir -p $base_path/zdoom/filter/doom.doom2.plutonia/hires
+mkdir -p $base_path/zdoom/filter/doom.doom2.tnt/hires
 
 echo ---------copying textures---------
-cp -r $base_path/textures $base_path/zdoom/textures
-mv $base_path/zdoom/textures/doom1/* $base_path/zdoom/doom/hires
-mv $base_path/zdoom/textures/doom2/* $base_path/zdoom/doom2/hires
-mv $base_path/zdoom/textures/doom2-plut/* $base_path/zdoom/doom2.plutonia/hires
-mv $base_path/zdoom/textures/doom2-tnt/* $base_path/zdoom/doom2.tnt/hires
-rmdir $base_path/zdoom/textures/doom*
-mv $base_path/zdoom/textures/* $base_path/zdoom/doom/hires
+cp -r $base_path/textures $base_path/zdoom/filter/textures
+mv $base_path/zdoom/filter/textures/doom1/* $base_path/zdoom/filter/doom/hires
+mv $base_path/zdoom/filter/textures/doom2/* $base_path/zdoom/filter/doom.doom2/hires
+mv $base_path/zdoom/filter/textures/doom2-plut/* $base_path/zdoom/filter/doom.doom2.plutonia/hires
+mv $base_path/zdoom/filter/textures/doom2-tnt/* $base_path/zdoom/filter/doom.doom2.tnt/hires
+rmdir $base_path/zdoom/filter/textures/doom*
+mv $base_path/zdoom/filter/textures/* $base_path/zdoom/filter/doom/hires
 echo ---------copying flats---------
-cp -r $base_path/flats/* $base_path/zdoom/doom/hires
-
-pushd $base_path/zdoom/doom
-zip -r $base_path/zdoom/zdoom-dhtp-$datestamp-doom.pk3 . -i \*.png
-popd
-pushd $base_path/zdoom/doom2
-zip -r $base_path/zdoom/zdoom-dhtp-$datestamp-doom2.pk3 . -i \*.png
-popd
-pushd $base_path/zdoom/doom2.plutonia
-zip -r $base_path/zdoom/zdoom-dhtp-$datestamp-doom2.plutonia.pk3 . -i \*.png
-popd
-pushd $base_path/zdoom/doom2.tnt
-zip -r $base_path/zdoom/zdoom-dhtp-$datestamp-doom2.tnt.pk3 . -i \*.png
-popd
-
+cp -r $base_path/flats/* $base_path/zdoom/filter/doom/hires
 
 echo ---------copying readmes---------
 for filename in "$base_path/docs/"*; do
@@ -47,7 +33,7 @@ done
 
 echo ---------ziping pack---------
 pushd $base_path/zdoom
-zip -r $base_path/built/zdoom-dhtp-$datestamp.zip . -i \*.pk3 \*.txt
+zip -r $base_path/built/zdoom-dhtp-$datestamp.pk3 . -i \*.png \*.txt
 popd
 
 echo ---------Complete---------
